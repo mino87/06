6da277c3fd36ab1a6ad978a6e19cc42d51fa8150
mino87 <113091109+mino87@users.noreply.github.com>
2022-10-11 17:05:56 +0900

    04

--- main.c
+++ main.c
@@ -5,42 +5,17 @@
 
 int main(int argc, char *argv[]) {
 	
-	int sumTwo(int a, int b)
-	{
-		int output;
-		output = a+b;
-		return output;
-	}
-	
-	int sum = sumTwo(2,3);
-	printf("%d",sum);
-	
-	
-	int square (int n)
-	{
-	   int output;
-	   output =n*n;
-	   return  output;
-	}
-	
-	int c=square(5);	
-	printf("%d",c);
-	
-	int get_max(int x, int y)
-	{
-		if(x>y) return (x);
-		else return(y);	                     
-	}
-	
-int res_sum= sumTwo(2,3);
-int res_square=square(5);
-int res_max=get_max(5,7);
+int square(int a)
+{
+    return (a*a);
+}	
 
-printf("%d%d%d",res_sum,res_square,res_max);
-
-  
-	
-	
-	
-	return 0;
+int main()
+{
+	int a = 2;
+	a = square(a);
+	printf("a=%i\n",a);
 }
+}
+	
+	
