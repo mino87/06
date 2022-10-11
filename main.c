a97b2c82dc8451170f5c1a09bd19ceb12808ae09
mino87 <113091109+mino87@users.noreply.github.com>
2022-10-11 21:31:55 +0900

    05

--- main.c
+++ main.c
@@ -5,17 +5,51 @@
 
 int main(int argc, char *argv[]) {
 	
-int square(int a)
-{
-    return (a*a);
-}	
+int combination();
+int fac(int n);
+int get_integer();
 
-int main()
+int get_integer();
+
 {
-	int a = 2;
-	a = square(a);
-	printf("a=%i\n",a);
+	printf("정수를 입력하십시오 : ");
+	int a;
+	scanf("%d,&a");
+	int input =(a); 
+	return a;
 }
+
+int combination(int b, int c);
+{
+	int b,c;
+	int result=fac(b)/(fac(c)*fac(b-c));
+	return result;
+	printf("%d",result); 
+	
 }
-	
-	
+
+int fac(int n);
+{
+	int n;
+	int res=1;
+	int i;
+	for (i=1; i<n; i++)
+	   res=res*i;
+	return res;
+ } 
+int j,k;
+j=get_integer();
+k=get_integer();
+
+int y;
+y=combination(j,k);
+
+
+
+
+
+
+
+	return 0;
+}
+
